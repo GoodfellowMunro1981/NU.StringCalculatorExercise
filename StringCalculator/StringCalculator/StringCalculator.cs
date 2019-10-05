3c3482d53d9a4c88f8a8c29d0cff6f11328714e6
John Munro <goodfellow_munro@hotmail.com>
2019-10-05 22:46:53 +0100

    Step 1 [Changes complete]
    Create a simple String calculator with a method int Add(string numbers)
    • The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
    • Start with the simplest test case of an empty string and move to one and two numbers

---
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator
+{
+    public static class StringCalculator
+    {
+        private static char[] DEFAULT_DELIMITERS = new char[] { ',' };
+
+        public static int Add(string numbers)
+        {
+            int total = 0;
+
+            if(!string.IsNullOrEmpty(numbers))
+            {
+                string[] numberValues = numbers.Split(DEFAULT_DELIMITERS);
+
+                foreach (var numberValue in numberValues)
+                {
+                    if (int.TryParse(numberValue, out int number))
+                    {
+                        total += number;
+                    }
+                }
+            }
+
+            return total;
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator.Tests
+{
+    [TestClass()]
+    public class StringCalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest_EmptyString_Success()
+        {
+            // Arrange 
+            var numbers = default(string);
+
+            // Act
+            var result = StringCalculator.Add(numbers);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void AddTest_SingleNumberString_Success()
+        {
+            // Arrange 
+            var numbers = "1";
+
+            // Act
+            var result = StringCalculator.Add(numbers);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod()]
+        public void AddTest_DoubleNumberString_Success()
+        {
+            // Arrange 
+            var numbers = "1, 2";
+
+            // Act
+            var result = StringCalculator.Add(numbers);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod()]
+        public void AddTest_TrebleNumberString_Success()
+        {
+            // Arrange 
+            var numbers = "1, 2, 3";
+
+            // Act
+            var result = StringCalculator.Add(numbers);
+
+            // Assert
+            Assert.AreEqual(6, result);
+        }
+    }
+}
